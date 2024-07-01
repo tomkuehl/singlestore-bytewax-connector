6c03f870c413ad74558213ce491adbb9c4b218ef
Tom Kühl <hello@tomkuehl.io>
2024-07-01 22:45:10 +0200

    fix: Use async mysql lib to avoid main thread block

--- src/singlestore_source.py
+++ src/singlestore_source.py
@@ -1,8 +1,10 @@
 from typing import Iterable, List, Optional
-from bytewax.inputs import StatefulSourcePartition, FixedPartitionedSource
+from bytewax.inputs import StatefulSourcePartition, FixedPartitionedSource, batch_async
 from binascii import hexlify
-import MySQLdb
-from MySQLdb.cursors import SSCursor
+from asyncmy import connect
+from asyncmy.cursors import SSCursor
+from datetime import timedelta
+import asyncio
 
 
 class SingleStoreSource(FixedPartitionedSource[dict, dict[int, Optional[bytes]]]):
@@ -23,16 +25,20 @@ class SingleStoreSource(FixedPartitionedSource[dict, dict[int, Optional[bytes]]]
             "BeginSnapshot",
             "CommitSnapshot",
         ],
+        batch_size: int = 100,
+        batch_timeout: timedelta = timedelta(seconds=1),
     ):
         self.connection_params = {
             "host": host,
             "port": port,
             "user": user,
-            "passwd": password,
-            "db": database,
+            "password": password,
+            "database": database,
         }
         self.table = table
         self.event_types = event_types
+        self.batch_size = batch_size
+        self.batch_timeout = batch_timeout
 
     def list_parts(self) -> List[str]:
         return [self.table]
@@ -44,7 +50,12 @@ class SingleStoreSource(FixedPartitionedSource[dict, dict[int, Optional[bytes]]]
         resume_state: Optional[dict[int, Optional[bytes]]],
     ) -> StatefulSourcePartition[dict, dict[int, Optional[bytes]]]:
         return _SingleStoreSourcePartition(
-            self.connection_params, for_part, resume_state, self.event_types
+            self.connection_params,
+            for_part,
+            resume_state,
+            self.event_types,
+            self.batch_size,
+            self.batch_timeout,
         )
 
 
@@ -57,51 +68,76 @@ class _SingleStoreSourcePartition(
         table: str,
         resume_state: Optional[dict[int, Optional[bytes]]],
         event_types: List[str],
+        batch_size: int,
+        batch_timeout: timedelta,
     ):
         self.connection_params = connection_params
         self.table = table
         self.event_types = event_types
         self.last_offsets = {
             i: resume_state[i] if resume_state and i in resume_state else None
-            for i in range(self._get_partition_count())
+            for i in range(asyncio.run(self._get_partition_count()))
         }
-        self.connection, self.cursor = self._start_observe_query()
+        self.loop = asyncio.new_event_loop()
+        self.batcher = batch_async(
+            self._observe(),
+            timeout=batch_timeout,
+            batch_size=batch_size,
+            loop=self.loop,
+        )
 
     def next_batch(self) -> Iterable[dict]:
-        row = self.cursor.fetchone()
-        if row is None:
-            return []
-        self.last_offsets[row[1]] = row[0]
-        if row[2] in self.event_types:
-            yield {"type": row[2], "data": row[7:]}
+        return next(self.batcher)
 
     def snapshot(self) -> dict[int, Optional[bytes]]:
         return self.last_offsets
 
     def close(self):
-        self.connection.close()
-        self.cursor.close()
+        async def cleanup():
+            if self.cursor:
+                try:
+                    async with connect(**self.connection_params) as kill_connection:
+                        async with kill_connection.cursor() as kill_cursor:
+                            await kill_cursor.execute(
+                                f"KILL QUERY {self.connection_id}"
+                            )
+                except Exception:
+                    print(f"Failed to kill query {self.connection_id}")
+                await self.cursor.close()
+            if self.connection:
+                await self.connection.close()
 
-    def _start_observe_query(self):
-        connection = MySQLdb.connect(**self.connection_params)
-        cursor = connection.cursor(SSCursor)
+        self.loop.run_until_complete(cleanup())
+        self.loop.close()
+
+    async def _observe(self):
         query = f"OBSERVE * FROM {self.table}"
-        offsets = self._restore_offsets()
+        offsets = self._stringify_offsets()
         if offsets:
             query += f" BEGIN AT ({','.join(offsets)})"
-        cursor.execute(query)
-        return connection, cursor
+        self.connection = await connect(**self.connection_params)
+        async with self.connection.cursor() as id_cursor:
+            await id_cursor.execute("SELECT CONNECTION_ID()")
+            self.connection_id = (await id_cursor.fetchone())[0]
 
-    def _restore_offsets(self):
+        self.cursor = self.connection.cursor(SSCursor)
+        await self.cursor.execute(query)
+        while True:
+            row = await self.cursor.fetchone()
+            self.last_offsets[row[1]] = row[0]
+            if row[2] in self.event_types:
+                yield {"type": row[2], "data": row[7:]}
+
+    def _stringify_offsets(self):
         ordered_offsets = sorted(self.last_offsets.items(), key=lambda x: x[0])
         return [
             f"'{hexlify(offset).decode('utf-8')}'" if offset is not None else "NULL"
             for _, offset in ordered_offsets
         ]
 
-    def _get_partition_count(self):
-        with MySQLdb.connect(**self.connection_params) as connection:
-            with connection.cursor() as cursor:
-                cursor.execute("SHOW PARTITIONS")
-                partitions = cursor.fetchall()
+    async def _get_partition_count(self):
+        async with connect(**self.connection_params) as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute("SHOW PARTITIONS")
+                partitions = await cursor.fetchall()
                 return len(partitions) if partitions is not None else 1
